83ebf48e08b2f74965114377fa3353448204cd9b
eliab256 <bordonielia96@gmail.com>
2025-03-16 22:06:52 +0000

    initializing contracts

--- script/DeployYoyoNft.s.sol
+++ script/DeployYoyoNft.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {YoyoNft} from "../src/YoyoNFT.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract YoyoNftScript is Script {
+    YoyoNft public yoyoNft;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        yoyoNft = new YoyoNft();
 
         vm.stopBroadcast();
     }
---
+++ test/YoyoNFT.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import {YoyoNft} from "../src/YoyoNFT.sol";
+
+contract YoyoNftTest is Test {
+    YoyoNft public yoyoNft;
+}
