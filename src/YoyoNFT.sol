7800b2a7283e24cab6e879bf3ac144dcf7943213
eliab256 <bordonielia96@gmail.com>
2025-03-18 14:32:05 +0000

    adding mapping for uris

--- src/YoyoNFT.sol
+++ src/YoyoNFT.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract YoyoNft {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract YoyoNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("Yoyo Collection", "YOYO") {
+        s_tokenCounter = 0;
+    }
 }
