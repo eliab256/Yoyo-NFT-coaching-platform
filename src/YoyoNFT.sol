002b4f2c87490202706840af7abaa3a4d4dc1b2d
b-elia <bordonielia96@gmail.com>
2025-04-09 21:31:02 +0200

    adding string and fixing imports

--- src/YoyoNFT.sol
+++ src/YoyoNFT.sol
@@ -22,8 +22,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {console} from "forge-std/console.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {VRFConsumerBaseV2Plus, VRFV2PlusClient, IVFRCoordinatorV2Plus} from "@chainlink/contracts/src/v0.8";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {IVRFCoordinatorV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/interfaces/IVRFCoordinatorV2Plus.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
 
 /**
  * @title A Yoga NFT collection
